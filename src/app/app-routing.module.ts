f161b33e5c3653d619a139ba55d8d0b80c621d6a
NATALIALOZER <badwolf101115@gmail.com>
2021-11-26 18:13:37 +0200

    billing-page added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {BillingPageComponent} from './pages/billing-page/billing-page.component';
 import {AuthGuard} from './shared/guards/auth.guard';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
 
 const routes: Routes = [
   { path: '', component: MainLayoutComponent, canActivate: [AuthGuard], children: [
-      {path: 'billing-page', component: BillingPageComponent}
+      {path: 'billing-page', loadChildren: () => import('./pages/billing-page/billing-page.module').then(x => x.BillingPageModule)}
     ]},
   { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(x => x.AuthModule) },
   { path: '**', redirectTo: '' }
