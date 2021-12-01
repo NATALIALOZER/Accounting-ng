5a1cf4fdd054c3a51fb9385a2093a2c1116665e4
NATALIALOZER <badwolf101115@gmail.com>
2021-12-01 07:12:51 +0200

    added table sort

--- src/app/pages/history-page/table/table.component.ts
+++ src/app/pages/history-page/table/table.component.ts
@@ -1,17 +1,25 @@
-import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { EventInfo, RateTableData } from '../../../shared/models/interfaces';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
   @Input() public dataSource!: MatTableDataSource<EventInfo>;
+  @ViewChild(MatSort, {static: true}) public sort!: MatSort;
   public displayedColumns: string[] = ['id', 'amount', 'date', 'category', 'type', 'action'];
   public search: string = '';
+
+  public ngOnChanges(): void {
+    if (this.dataSource) {
+      this.dataSource.sort = this.sort;
+    }
+  }
 
   public applyFilter(): void {
     this.dataSource.filter = this.search.trim().toLowerCase();
