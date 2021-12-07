f0db1826b386a8b402e03fd894fea5ccab39dfe1
NATALIALOZER <badwolf101115@gmail.com>
2021-12-07 14:21:09 +0200

    added record page table

--- src/app/pages/record-page/record-page.component.ts
+++ src/app/pages/record-page/record-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ICategory } from '../../shared/models/interfaces';
+import { Subject, takeUntil } from 'rxjs';
+import { DbProfileInfoService } from '../../shared/services/db-profile-info.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-record-page',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record-page.component.scss']
 })
 export class RecordPageComponent implements OnInit {
+  @ViewChild(MatSort, {static: true}) public sort!: MatSort;
+  public dataSource!: MatTableDataSource<ICategory>;
+  public data: ICategory[] = [];
+  public displayedColumns: string[] = ['id', 'name', 'capacity', 'action'];
+  private destroy$: Subject<void> = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private profileInfoService: DbProfileInfoService
+  ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.getCat();
   }
 
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private getCat(): void {
+    this.profileInfoService.getCategories()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (response: ICategory[]) => {
+          this.data = response;
+          this.dataSource = new MatTableDataSource<ICategory>(this.data);
+          this.dataSource.sort = this.sort;
+        });
+  }
 }
